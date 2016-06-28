9a65abdb6a63eb71454371a2e1491541390cd2a9
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2016-06-28 17:50:53 +0000

    Add files via upload

---
+++ CarteleraController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cine.AppWeb.CineWS;
+
+namespace Cine.AppWeb.Controllers
+{
+    public class CarteleraController : Controller
+    {
+        TransaccionesClient proxy = new TransaccionesClient();
+        //
+        // GET: /Cartelera/
+        public ActionResult Index()
+        {
+            var listado = proxy.ListarCartelera();
+            return View(listado);
+        }
+
+
+        public ActionResult Crear()
+        {
+            var proxy = new TransaccionesClient();
+            var listarPelicula = proxy.ListarPelicula();
+            ViewBag.Idpelicula = new SelectList(listarPelicula, "idPelicula", "titulo");
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Crear(Cartelera carte, int idpelicula)
+        {
+            var proxy = new TransaccionesClient();
+            var peli = proxy.obtenerPelicula(idpelicula);
+            carte.pelicula = peli;
+            proxy.crearCartelera(carte);
+            return RedirectToAction("Index");
+
+        }
+	}
+}
