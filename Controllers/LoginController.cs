6826857a42605ec58b442faab939c32ee6988814
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2016-07-05 12:27:21 -0500

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cine.AppWeb.CineWS;
+
+namespace Cine.AppWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        TransaccionesClient proxy =new  TransaccionesClient();
+        //
+        // GET: /Login/
+      public ActionResult Iniciar()
+        {
+
+            return View();
+        }
+
+         [HttpPost]
+        public ActionResult Iniciar(Colaborador colabo)
+        {
+            TransaccionesClient proxy = new TransaccionesClient();
+
+            bool estado = proxy.IniciarSesionColaborador(colabo);
+            Colaborador colaborador = new Colaborador();
+            int idco = colaborador.idColaborador;
+            //colaborador = proxy.obtenerColaborador(idco);
+            //int pos = colaborador.idColaborador;
+            string men="";
+            string controlador = "";
+
+            if (estado == true)
+            {
+                men = "Index";
+                controlador = "Home";
+                
+            }
+            else
+            {
+                
+                men = "Iniciar";
+                controlador = "Login";
+                
+            }
+
+            return RedirectToAction(men, controlador, new { mensaje = idco });
+        }
+    }
+}
