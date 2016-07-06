124f3bbb17d000f9a96219940fc68c8cc975376c
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2016-07-06 12:33:06 -0500

    Add files via upload

---
+++ Cine.AppWebCliente/Cine.AppWenCliente/Cine.AppWenCliente/Scripts/formatDate.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $(("input[id$='TextBox1']")).datepicker({
+        dateFormat: 'yy/mm/dd'
+    });
+    $(("input[id$='TextBox2']")).datepicker({
+        dateFormat: 'yy/mm/dd'
+    });
+});
