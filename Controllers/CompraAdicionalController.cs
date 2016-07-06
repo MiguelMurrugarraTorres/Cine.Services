4c51a4e7f81fab1b7002340fb0be3786eb5842c5
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2016-07-06 12:25:30 -0500

    Add files via upload

---
+++ Controllers/CompraAdicionalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cine.AppWeb.CineWS;
+namespace Cine.AppWeb.Controllers
+{
+    public class CompraAdicionalController : Controller
+    {
+        TransaccionesClient proxy = new TransaccionesClient();
+        //
+        // GET: /CompraAdicional/
+        public ActionResult Index()
+        {
+            var listado = proxy.ListarCompraAdicional();
+            return View(listado);
+        }
+	}
+}
