cd9e0caf7776af8b86871f4b42f560db78b0d0b1
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2016-07-06 12:31:08 -0500

    Add files via upload

---
+++ Cine.AppWebCliente/Cine.AppWenCliente/Cine.AppWenCliente/Controllers/EstrenosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cine.AppWenCliente.CineWS;
+
+namespace Cine.AppWenCliente.Controllers
+{
+    public class EstrenosController : Controller
+    {
+        TransaccionesClient proxy = new TransaccionesClient();
+        //
+        // GET: /Estrenos/
+        public ActionResult Index()
+        {
+            var listado = proxy.ListarEstrenos();
+            return View(listado);
+        }
+	}
+}
