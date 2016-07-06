c21195919fe9890fbff8d8f162b61f0f4c1ddc45
MiguelMurrugarraTorres <Angel.16.92@hotmail.com>
2016-07-06 12:25:05 -0500

    Add files via upload

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cine.AppWeb.Controllers
+{
+    public class MenuController : Controller
+    {
+        //
+        // GET: /Menu/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
